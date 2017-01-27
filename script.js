358ed5f0a9df79c9bdbd37ca287ae052c7e1c478
jvalentine12 <jvalentine@cristoreyny.org>
2017-01-27 14:00:05 +0000

    works

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
-    $("#go").click(function(){
-        
+    $("#button").click(function(){
+    
         var searchval = $("#search").val();
+        console.log(searchval)
         var part1 = "https://api.foursquare.com/v2/venues/search?"
-        var part2 = "client_id=102GA0KW5LFMNCIFOB4I3GRTZ0IEODRDS31RRLSJALEV1SCM&client_secret=DIKMWCOXGUZK3N3TKXV1Z3LOE40WLKT4AEHBEQQFJA5JL5SO&v=20130815&near=40.7,-74&query="
-        var URL = part1 + part2 + searchval
+        var part2 = "client_id=102GA0KW5LFMNCIFOB4I3GRTZ0IEODRDS31RRLSJALEV1SCM&client_secret=DIKMWCOXGUZK3N3TKXV1Z3LOE40WLKT4AEHBEQQFJA5JL5SO&"
+        var part3 = "&v=20130815&ll=40.7,-74&query="
+        var URL = part1 + part2 + part3 + searchval ;
         
         $.getJSON(URL,function(response){
-            var name = response.
-            var phone = response.
-            var address = response.
-            $("#results").append()
+           console.log(response) 
+            var name = response.venues.name
+            var phone = response.venues.contact.phone
+            var address = response.venues.location.address
+            $("#results").append("<br>" + name + "<br>" + phone + "<br>" + address);
         });
     });
 });
