846afeae46272501e342a1a0b93c12753b4c6e5e
jvalentine12 <jvalentine@cristoreyny.org>
2017-02-02 13:34:18 +0000

    improved 2

--- script.js
+++ script.js
@@ -7,14 +7,13 @@ $(document).ready(function(){
         var part2 = "client_id=102GA0KW5LFMNCIFOB4I3GRTZ0IEODRDS31RRLSJALEV1SCM&client_secret=DIKMWCOXGUZK3N3TKXV1Z3LOE40WLKT4AEHBEQQFJA5JL5SO&"
         var part3 = "v=20130815&near=40.7,-74&query="
         var URL = part1 + part2 + part3 + searchval ;
-        alert(URL);
+        
         $.getJSON(URL,function(response){
-           
+           console.log(response)
             var name = response.venues.name[0]
             var phone = response.venues.contact.phone
             var address = response.venues.location.address
             $("#results").append("<br>" + name + "<br>" + phone + "<br>" + address);
-            console.log(response)
         });
     });
 });
