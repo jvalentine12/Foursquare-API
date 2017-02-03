d4bc87789c223b5e8762ea3987bd727faf99c4f4
jvalentine12 <jvalentine@cristoreyny.org>
2017-02-03 13:40:00 +0000

    improved 3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#button").click(function(){
     
         var searchval = $("#search").val();
-        console.log(searchval)
+        
         var part1 = "https://api.foursquare.com/v2/venues/search?"
         var part2 = "client_id=102GA0KW5LFMNCIFOB4I3GRTZ0IEODRDS31RRLSJALEV1SCM&client_secret=DIKMWCOXGUZK3N3TKXV1Z3LOE40WLKT4AEHBEQQFJA5JL5SO&"
         var part3 = "v=20130815&near=40.7,-74&query="
@@ -10,10 +10,11 @@ $(document).ready(function(){
         
         $.getJSON(URL,function(response){
            console.log(response)
-            var name = response.venues.name[0]
-            var phone = response.venues.contact.phone
-            var address = response.venues.location.address
+            var name = response.response.venues[0].name
+            var phone = response.response.venues[0].contact.phone
+            var address = response.response.venues[0].location.address
             $("#results").append("<br>" + name + "<br>" + phone + "<br>" + address);
+            
         });
     });
 });
